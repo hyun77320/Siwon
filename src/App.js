5268cadea2c30b8f6de4b106587229fbebea7c35
iqamac <hyun77320@naver.com>
2022-04-05 23:21:21 +0900

    2022.04.05.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, {useEffect} from "react";
+import React from 'react';
 
-const {kakao} = window
-
-function App() {
-  useEffect(() => {
-    let container = document.getElementById('map');
-		let options = {
-			center: new kakao.maps.LatLng(33.450701, 126.570667),
-			level: 3
-		};
-
-		let map = new kakao.maps.Map(container, options);
-  })
-  
+const App = () => {
   return (
-    <div id="map" style={{width: '100%', height: '100vh'}}></div>
+    <div>
+      
+    </div>
   );
-}
+};
 
 export default App;
